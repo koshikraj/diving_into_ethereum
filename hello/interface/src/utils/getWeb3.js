628993c2aafbff16d41577acc3cd15c3914cc7f4
koshikraj <koshik.raj@gmail.com>
2019-10-20 09:08:30 +0530

    modifies configs

--- hello/interface/src/utils/getWeb3.js
+++ hello/interface/src/utils/getWeb3.js
@@ -1,5 +1,5 @@
 import Web3 from "web3";
- 
+
 const getWeb3 = () =>
   new Promise((resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
@@ -26,7 +26,7 @@ const getWeb3 = () =>
       // Fallback to localhost; use dev console port by default...
       else {
         const provider = new Web3.providers.HttpProvider(
-          "http://127.0.0.1:9545"
+          "http://127.0.0.1:8545"
         );
         const web3 = new Web3(provider);
         console.log("No web3 instance injected, using Local web3.");
@@ -34,5 +34,5 @@ const getWeb3 = () =>
       }
     });
   });
- 
+
 export default getWeb3;
